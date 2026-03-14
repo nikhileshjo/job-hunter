f6a9e7601c0ad3d2a09cd84eec5d0b5d1b46479b
nikhilesh <nikhilesh.joshi.1999@gmail.com>
2026-03-14 17:11:09 +0530

    ADD: Added datawarehouse DDLs

---
+++ init_ddl/br_mathco.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS br_mathco (
+	job_url VARCHAR(1000),
+	job_id VARCHAR(100),
+	job_role VARCHAR(100),
+	job_description VARCHAR(100000),
+	skills VARCHAR(1000),
+	location VARCHAR(100),
+	contact_person VARCHAR(1000),
+	education_requirement VARCHAR(1000),
+	extract_date VARCHAR(10)
+);
